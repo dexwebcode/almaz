17209fdd411157d0c1c2be2d30de50e882fce171
DEXWEBCODE <89159662+dexwebcode@users.noreply.github.com>
2026-07-06 07:32:21 +0000

    first commit

---
+++ equations.py
@@ -0,0 +1,53 @@
+# ФАЙЛ: EQUATIONS 
+# СТРУКТУРА(кратко):
+#     derivative_mass_to_time ------------> Вычисление зависимости массы от времени. 
+#     derivative_fuel_to_time ------------> Вычисление зависимости топлива от времени. 
+#     derivative_velocity_to_density -----> Вычисление зависимости скорости от плотности воздуха
+#     derivative_velocity_to_time() ------> Вычисление зависимости скорости от времени.  
+#     derivative_angle_tett_to_time() ----> Вычисление зависимости угла θ от времени
+#     derivative_X_to_time() -------------> Вычисление зависимости угла X от времени
+#     derivative_Z_to_time() -------------> Вычисление зависимости угла 𝑍 от времени
+#     is_straight_flight() ---------------> Вычисление условия движения без рыскания
+#     air_density_at_height() ------------> Вычисление плотности воздуха на высоте
+
+from constants import *
+        
+""" Вычисление зависимости массы от времени. """
+def derivative_mass_to_time(a):
+    pass
+
+""" Вычисление зависимости топлива от времени. """
+def derivative_fuel_to_time(a):
+    pass
+
+""" Вычисление зависимости скорости от плотности воздуха. """
+def derivative_velocity_to_density():
+    pass
+
+""" Вычисление зависимости скорости от времени. """
+def derivative_velocity_to_time():
+    pass
+
+""" Вычисление зависимости угла θ от времени """
+def derivative_angle_tett_to_time():
+    pass
+
+""" Вычисление зависимости угла X от времени"""
+def derivative_X_to_time(self):
+    pass
+
+""" Вычисление зависимости высоты от времени"""
+def derivative_height_to_time(self):
+    pass
+
+""" Вычисление зависимости угла 𝑍 от времени """
+def derivative_Z_to_time(self):
+    pass
+
+""" Вычисление условия движения без рыскания """
+def is_straight_flight(self):
+    pass
+
+""" Вычисление плотности воздуха на высоте """
+def air_density_at_height(self):
+    pass   
