1ea326cee1ed2d339d1a557f72704a119f5f1614
DEXWEBCODE <89159662+dexwebcode@users.noreply.github.com>
2026-07-06 13:39:37 +0000

    Первая версия решения системы уравнения методом Эйлера

--- constants.py
+++ constants.py
@@ -1,6 +1,22 @@
-fuel_consumption_per_second = 2 # Расход топлива в секунду кг/c
+# ФАЙЛ: constants
+# КОМЕНТАРИЙ: Хранит переменные для посчета
+
+
+FUEL_CONSUMPION = 2 # Расход топлива в секунду кг/c
 k = 1.5 # Табличное значение 
-g = 9.81 # Скорость свуободног падения
+G = 9.81 # Скорость свуободног падения
 h = 0.001 # Шаг времени
-initial_mass = 100 # Начальная масса
-initial_time = 0 # Начальное время
+INITIAL_MASS = 100 # Начальная масса
+INITIAL_TIME = 0 # Начальное время
+INITIAL_FUEL = 4000 # Начальное топливо
+END_TIME = 1 # Конечное время
+COEF_K = 1.5
+SQUARE = 0.00785
+
+INITIAL_H = 0
+INITIAL_TETHA = 45
+INITIAL_X = 0
+INITIAL_V = 0
+INITAL_Z = 0
+
+RHO_AT_SEA_LEVEL = 1.225
